cacafef7a0f015fe89f4696a0ce99b44ba5bd549
Michael Stone <m@mstone.info>
2022-12-20 10:36:01 -0500

    impl: add proc-macro crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+//! # wye
+//! 
+//! wye is a crate for instrumenting Rust code to record causal traces for debugging.
+//! 
+//! Consider a simple program:
+//! 
+//! fn add(a: u64, b: u64) -> u64 {
+//!     a + b
+//! }
+//! 
+//! fn example() -> bool {
+//!     let result = add(1, 2);
+//!     result == 3
+//! }
+//! 
+//! A causal trace for the execution of `add(...)` in example might be:
+//! 
+//! digraph {
+//!     1 [ label="a = 1" ];
+//!     2 [ label="b = 2" ];
+//!     3 [ label="+" ];
+//!     4 [ label="ret = 3" ];
+//!     1 -> 3;
+//!     2 -> 3;
+//!     3 -> 4;
+//! }
+//! 
+//! # See Also
+//! 
+//! * [rr](https://rr-project.org)
+//! * [PANDA](https://github.com/panda-re/panda)
+//! * [pernosco](https://pernos.co)
+
 pub mod log {
     use petgraph::graph::Graph;
     pub struct Logger {
@@ -13,6 +46,14 @@ pub mod log {
     }
 }
 
+pub mod expr {
+
+}
+
+pub mod iter {
+
+}
+
 pub mod collections {
     pub struct Vec {}
 
@@ -25,4 +66,3 @@ pub mod collections {
     pub struct BTreeSet {}
 
 }
-
