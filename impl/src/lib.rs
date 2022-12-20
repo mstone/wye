409a1df84df187655822266d2ea95a42f55954d5
Michael Stone <m@mstone.info>
2022-12-20 15:38:20 -0500

    impl: simplify use stmt generation via parse_quote!

--- impl/src/lib.rs
+++ impl/src/lib.rs
@@ -1,6 +1,5 @@
-use proc_macro2::{Span};
 use quote::{ToTokens};
-use syn::{parse_macro_input, Item, Token};
+use syn::{parse_macro_input, parse_quote, Item};
 
 fn process_item(item: &mut Item) {
     if let Item::Fn(item_fn) = item {
@@ -22,18 +21,7 @@ fn process_sig_stmts(sig: &mut syn::Signature, stmts: &mut Vec<syn::Stmt>) {
     let syn::Signature{inputs, output, ..} = sig;
     let _ = inputs;
     let _ = output;
-    let use_stmt = syn::Stmt::Item(syn::Item::Use(syn::ItemUse{
-        attrs: vec![],
-        vis: syn::Visibility::Inherited,
-        use_token: Token![use](Span::call_site()),
-        leading_colon: None,
-        tree: syn::UseTree::Path(syn::UsePath{
-            ident: syn::Ident::new("wye", Span::call_site()),
-            colon2_token: syn::Token![::](Span::call_site()),
-            tree: Box::new(syn::UseTree::Name(syn::UseName{ ident: syn::Ident::new("arg", Span::call_site()) }))
-        }),
-        semi_token: Token![;](Span::call_site())
-    }));
+    let use_stmt: syn::Stmt = parse_quote!(use wye_impl::arg;);
     stmts.insert(0, use_stmt);
 }
 
@@ -44,4 +32,10 @@ pub fn wye(args: proc_macro::TokenStream, input: proc_macro::TokenStream) -> pro
     process_item(&mut input);
     let tokens = input.into_token_stream();
     tokens.into()
+}
+
+#[proc_macro_attribute]
+pub fn arg(args: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let _ = args;
+    input
 }
