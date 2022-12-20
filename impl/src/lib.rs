26fb6d5f42eca7fe487d37c7e14c5f9e89113e6c
Michael Stone <m@mstone.info>
2022-12-20 14:47:41 -0500

    impl: add syn + quote deps

--- impl/src/lib.rs
+++ impl/src/lib.rs
@@ -1,6 +1,10 @@
+use quote::{ToTokens};
+use syn::{parse_macro_input, Item};
 
 #[proc_macro_attribute]
 pub fn wye(args: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let _ = args;
-    input
+    let input = parse_macro_input!(input as Item);
+    let tokens = input.into_token_stream();
+    tokens.into()
 }
