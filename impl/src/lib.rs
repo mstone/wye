04ea5a8a6269916e1bbc61e0f76570bf7432553c
Michael Stone <m@mstone.info>
2022-12-20 13:52:58 -0500

    impl: tweak signature

--- impl/src/lib.rs
+++ impl/src/lib.rs
@@ -1,7 +1,6 @@
-use proc_macro::{TokenStream};
 
 #[proc_macro_attribute]
-pub fn wye(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn wye(args: proc_macro::TokenStream, input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let _ = args;
     input
 }
