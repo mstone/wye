57fa281d392dc307bd2a1e085532d10378b59910
Michael Stone <m@mstone.info>
2022-12-25 16:40:25 -0500

    wye: fix doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -10,59 +10,28 @@
 //! }
 //! 
 //! fn example() -> bool {
-//!     add(1, add(2, 3)) == 6
+//!     wyre!{add(1, add(2, 3))} == 6
 //! }
 //! 
 //! A causal trace for the execution of `add(...)` in example might be presented by:
 //! 
 //! digraph {
-//!     1 [ label="a = 2" ];
-//!     2 [ label="b = 3" ];
-//!     3 [ label="+" ];
-//!     4 [ label="5" ];
-//!     5 [ label="a = 1"];
-//!     6 [ label="b = 5"];
-//!     7 [ label="+"];
-//!     8 [ label="6"];
-//!     1 -> 3;
-//!     2 -> 3;
-//!     3 -> 4;
-//!     4 -> 6;
-//!     5 -> 7;
-//!     6 -> 7;
-//!     7 -> 8;
+//! 0 [ label = "a = 2" ]
+//! 1 [ label = "b = 3" ]
+//! 2 [ label = "+" ]
+//! 3 [ label = "5" ]
+//! 4 [ label = "a = 1" ]
+//! 5 [ label = "b = 5" ]
+//! 6 [ label = "+" ]
+//! 7 [ label = "6" ]
+//! 0 -> 2 [ label = "" ]
+//! 1 -> 2 [ label = "" ]
+//! 2 -> 3 [ label = "" ]
+//! 3 -> 5 [ label = "" ]
+//! 4 -> 6 [ label = "" ]
+//! 5 -> 6 [ label = "" ]
+//! 6 -> 7 [ label = "" ]
 //! }
-//! 
-//! To produce this, we need to transform add to
-//! 
-//! fn add(a: u64, b: u64) -> u64 {
-//!     let ~w = wye_ref(WYE);
-//!     let a = wye!{a}; // let a = wye::Value(a);
-//!     let b = wye!{b}; // let b = wye::Value(b);
-//!     let r1 = a + b;
-//!     let l1 = wye_place!{};
-//!     l1.set(r1);
-//!     l1.into();
-//! }
-//! 
-//! which when executed, needs to produce
-//! 
-//! let a = w.node("a", arg!{a});
-//! let b = w.node("b", arg!{b});
-//! let r1 = w.node("", "+");
-//! let t1 = 2 + 3;
-//! let l1 = w.node("", r1)
-//! w.edge(a, o1);
-//! w.edge(b, o1);
-//! w.edge(r1, l1);
-//! ret!{l1};
-//! 
-//! where arg! has the effect of determining whether or not the value in a is the result of a ret! or a set!, and where
-//! ret! has the effect of registering l1 as a non-root (such that subsequent calls to arg! or get! will connect the
-//! resulting graph.
-//! 
-//! (This seems like something that can maaaaybe be done either by wye! doing something fancy with function calls, or by
-//! registering the addresses of places used by ret!/set!? Or the source locations?)
 //! 
 //! # See Also
 //! 
